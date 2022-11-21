bfc2b1ad94bdec0875a548ec3ccfc21961106793
Dynamite1847 <yudong1864@gmail.com>
2022-11-21 21:56:37 +0800

    Add code

--- spark-core/src/main/java/com/learnBigData/spark/core/rdd/builder/RDDCoGroup.java
+++ spark-core/src/main/java/com/learnBigData/spark/core/rdd/builder/RDDCoGroup.java
@@ -5,11 +5,9 @@ import org.apache.hadoop.thirdparty.com.google.common.collect.Lists;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
 import scala.Tuple2;
 
-public class RDDJoin {
+public class RDDCoGroup {
     public static void main(String[] args) {
         //准备环境
         SparkConf sparkConf = new SparkConf()
@@ -18,18 +16,17 @@ public class RDDJoin {
         JavaSparkContext sparkContext = new JavaSparkContext(sparkConf);
         JavaPairRDD<String, Integer> pairs = sparkContext.parallelizePairs(Lists.<Tuple2<String, Integer>>newArrayList(
                         new Tuple2<String, Integer>("a", 1),
-                        new Tuple2<String, Integer>("b", 2),
-                        new Tuple2<String, Integer>("c", 3)),2);
+                        new Tuple2<String, Integer>("b", 2)),
+                2);
         JavaPairRDD<String, Integer> pairs1 = sparkContext.parallelizePairs(Lists.<Tuple2<String, Integer>>newArrayList(
                         new Tuple2<String, Integer>("a", 4),
                         new Tuple2<String, Integer>("b", 5),
-                        new Tuple2<String, Integer>("c", 6)),
+                        new Tuple2<String, Integer>("c", 6),
+                        new Tuple2<String, Integer>("c", 7)
+                        ),
                 2);
 
-        //join: 相同数据源的数据，想同的key的value会连接在一起形成元组
-        //如果两个数据源中没有匹配上key，不会出现在结果中
-        //如果有多个key相同，会依次匹配，可能会出现数据累量几何增长，导致性能降低
-        JavaPairRDD<String, Tuple2<Integer,Integer>> reduceRDD = pairs.join(pairs1);
+        JavaPairRDD<String, Tuple2<Iterable<Integer>, Iterable<Integer>>> reduceRDD = pairs.cogroup(pairs1);
         reduceRDD.collect().forEach(System.out::println);
     }
 }
--- spark-core/src/main/java/com/learnBigData/spark/core/rdd/builder/RDDLeftOuterJoin.java
+++ spark-core/src/main/java/com/learnBigData/spark/core/rdd/builder/RDDLeftOuterJoin.java
@@ -5,11 +5,10 @@ import org.apache.hadoop.thirdparty.com.google.common.collect.Lists;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
-import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.Optional;
 import scala.Tuple2;
 
-public class RDDJoin {
+public class RDDLeftOuterJoin {
     public static void main(String[] args) {
         //准备环境
         SparkConf sparkConf = new SparkConf()
@@ -22,14 +21,10 @@ public class RDDJoin {
                         new Tuple2<String, Integer>("c", 3)),2);
         JavaPairRDD<String, Integer> pairs1 = sparkContext.parallelizePairs(Lists.<Tuple2<String, Integer>>newArrayList(
                         new Tuple2<String, Integer>("a", 4),
-                        new Tuple2<String, Integer>("b", 5),
-                        new Tuple2<String, Integer>("c", 6)),
+                        new Tuple2<String, Integer>("b", 5)),
                 2);
 
-        //join: 相同数据源的数据，想同的key的value会连接在一起形成元组
-        //如果两个数据源中没有匹配上key，不会出现在结果中
-        //如果有多个key相同，会依次匹配，可能会出现数据累量几何增长，导致性能降低
-        JavaPairRDD<String, Tuple2<Integer,Integer>> reduceRDD = pairs.join(pairs1);
+        JavaPairRDD<String, Tuple2<Integer, Optional<Integer>>> reduceRDD = pairs.leftOuterJoin(pairs1);
         reduceRDD.collect().forEach(System.out::println);
     }
 }
